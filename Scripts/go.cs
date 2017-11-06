157f2597513e80d2a37d2875517ace519d74f52f
manuelseromenho <a21210@ualg.pt>
2017-11-06 12:09:27 +0000

    Alteração da inserção de imagens (IMPORTANTE)
    Neste commit:
    - Foi adicionado um script para que de modo mais dinâmico sejam
    apresentadas as imagens consoante o nr de linhas e colunas.
    - As imagens continuam a ser carregadas dos Resources.

---
+++ Scripts/go.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class go : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		PlayerPrefs.SetString ("lastLoadedScene", SceneManager.GetActiveScene ().name);
+		SceneManager.LoadScene("matriz");
+	}
+
+}
---
+++ Scripts/testescript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testescript : MonoBehaviour {
+
+	public GameObject prefab;
+	public GameObject row;
+	public Texture2D texture;
+
+	int nrLinhas = 2;
+	int nrColunas = 3;
+
+	private List<GameObject> images;
+	// Use this for initialization
+	void Start () {
+		
+		this.images = new List<GameObject> ();
+		for (int i = 0; i < nrLinhas; i++) 
+		{
+			var rowGO = Instantiate (row, this.transform);
+			for (int j = 0; j < nrColunas; j++) 
+			{
+				var child = Instantiate (prefab, rowGO.transform);
+				child.GetComponent<RectTransform> ().localScale = new Vector3 (-1, 1, 1);
+				this.images.Add (child);
+			}
+		}
+
+		var nrResources = Resources.LoadAll<Texture2D>("");
+		for (int i = 0; i < nrResources.Length; i++) {
+			this.images [i].GetComponent<RawImage> ().texture = nrResources [i];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
